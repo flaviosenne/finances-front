b6d54382a4f95e3c5896323eb9ea8a8fbbe4bb4b
joao flavio <flaviosenne123@gmail.com>
2023-02-07 19:47:35 -0300

    adicionar relatorio no endpoint de relatorio seguindo o exemplo em pdf gerado pelo node

--- src/pages/Report/index.tsx
+++ src/pages/Report/index.tsx
@@ -1,12 +1,89 @@
 import React from 'react'
 import SideBar from '../../components/SideBar';
-import { Container } from "./styles";
+import { Container, Content, Title, SummaryContainer, Summary, Table } from "./styles";
 
 export default function Report() {
+
+    const mockResult = [
+        { date: '2022-01-12', status: 'Pendente', description: 'um tipo de despesafe rrgnrehgu kenveri', type: 'Expense', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-07-14', status: 'Pago', description: 'um tipo de r3ceita', type: 'Recep', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-02-12', status: 'Pendente', description: 'um tipo de despesa', type: 'Expense', value: '1000.12', category: 'categoria', bank: 'Santander' },
+        { date: '2022-04-12', status: 'Pago', description: 'um tipo de receita', type: 'Recep', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-02', status: 'Pendente', description: 'um tipo de despesa', type: 'Expense', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-09', status: 'Pendente', description: 'um tipo de receits', type: 'Recep', value: '90.00', category: 'categoria', bank: 'Santander' },
+        { date: '2022-04-12', status: 'Pago', description: 'um tipo de receita', type: 'Recep', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-02', status: 'Pendente', description: 'um tipo de despesa', type: 'Expense', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-09', status: 'Pendente', description: 'um tipo de receits', type: 'Recep', value: '90.00', category: 'categoria', bank: 'Santander' },
+        { date: '2022-04-12', status: 'Pago', description: 'um tipo de receita', type: 'Recep', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-02', status: 'Pendente', description: 'um tipo de despesa', type: 'Expense', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-09', status: 'Pendente', description: 'um tipo de receits', type: 'Recep', value: '90.00', category: 'categoria', bank: 'Santander' },
+        { date: '2022-04-12', status: 'Pago', description: 'um tipo de receita', type: 'Recep', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-02', status: 'Pendente', description: 'um tipo de despesa', type: 'Expense', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-09', status: 'Pendente', description: 'um tipo de receits', type: 'Recep', value: '90.00', category: 'categoria', bank: 'Santander' },
+        { date: '2022-04-12', status: 'Pago', description: 'um tipo de receita', type: 'Recep', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-02', status: 'Pendente', description: 'um tipo de despesa', type: 'Expense', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-09', status: 'Pendente', description: 'um tipo de receits', type: 'Recep', value: '90.00', category: 'categoria', bank: 'Santander' },
+        { date: '2022-04-12', status: 'Pago', description: 'um tipo de receita', type: 'Recep', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-02', status: 'Pendente', description: 'um tipo de despesa', type: 'Expense', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-09', status: 'Pendente', description: 'um tipo de receits', type: 'Recep', value: '90.00', category: 'categoria', bank: 'Santander' },
+        { date: '2022-04-12', status: 'Pago', description: 'um tipo de receita', type: 'Recep', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-02', status: 'Pendente', description: 'um tipo de despesa', type: 'Expense', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-09', status: 'Pendente', description: 'um tipo de receits', type: 'Recep', value: '90.00', category: 'categoria', bank: 'Santander' },
+        { date: '2022-04-12', status: 'Pago', description: 'um tipo de receita', type: 'Recep', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-02', status: 'Pendente', description: 'um tipo de despesa', type: 'Expense', value: '100', category: 'categoria', bank: 'Santander' },
+        { date: '2022-10-09', status: 'Pendente', description: 'um tipo de receits', type: 'Recep', value: '90.00', category: 'categoria', bank: 'Santander' },
+        { date: '2022-03-21', status: 'Pendente', description: 'um tipo de despesa', type: 'Expense', value: '100', category: 'categoria', bank: 'Santander' },
+    ]
     return (
         <Container>
             <SideBar />
-            <p>Relatório</p>
+            <Content>
+
+                <Title>  Relatório de Lançamentos Financeiro      </Title>
+                <SummaryContainer>
+
+                    <Summary>
+                        <p>Periodo de Lançamento: <span>3 meses</span></p>
+                        <p>Total de Receitas: <span>R$ 1426,00</span> </p>
+                        <p>Total de Despesas: <span>R$ 1026,00</span> </p>
+                        <p>Total: <span>R$ 400,00</span> </p>
+                    </Summary>
+
+                    <Summary>
+                        <p>Data: <span>07/02/2023</span></p>
+                        <p>Solicitante: <span>João Fávio</span> </p>
+                        <p>Situação: <span>Positivo</span> </p>
+                        <p>Porcentagem de gastos <span>71%</span> </p>
+                    </Summary>
+
+                </SummaryContainer>
+                <Table>
+                    <thead className='thead-dark'>
+                        <th>TIPO</th>
+                        <th>DATA</th>
+                        <th>STATUS</th>
+                        <th>DESCRIÇÃO</th>
+                        <th>CATEGORIA</th>
+                        <th>BANCO</th>
+                        <th>VALOR</th>
+                    </thead>
+                    {mockResult.map(release => (
+                        <tbody>
+                            <td>{release.type}</td>
+                            <td>{release.date}</td>
+                            <td>{release.status}</td>
+                            <td>{release.description}</td>
+                            <td>{release.category}</td>
+                            <td>{release.bank}</td>
+                            <td>{release.value}</td>
+                        </tbody>
+
+                    ))}
+
+                </Table>
+
+            </Content>
+
         </Container>
     )
 }
--- src/pages/Report/styles.ts
+++ src/pages/Report/styles.ts
@@ -1,3 +1,90 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+
+export const Container = styled.div`
+    display: flex;
+    min-height: 100vh;
+    background: white;
+`
+
+export const Content = styled.div`
+    width: 80%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 20px;
+    margin: 8px auto;
+    
+    `
+export const Title = styled.div`
+    width: 100%;
+    border-radius: 8px;
+    margin: 8px;
+    padding: 12px;
+    background: var(--primary);
+    text-align: center;
+    font-weight: 600;
+`
+
+export const SummaryContainer = styled.div`
+    padding: 12px;
+    width: 100%;
+    border-radius: 8px;
+    margin: 8px;
+    background: aquamarine;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    
+    @media(max-width: 700px){
+        flex-direction: column;
+    }
+`
+
+export const Summary = styled.div`
+    p{
+        padding: 4px;
+        margin: 4px 0;
+        font-weight: 600;
+    }
+    span{
+        font-weight: 300;
+    }
+`
+
+export const Table = styled.table`
+    max-width:100%;
+    display: block;
+    max-height: 400px;
+    margin: 8px auto;
+    border-spacing: 1;
+    border-style: solid;
+    border-width: 1px;
+    overflow-y: scroll;
+    
+    @media(max-width: 700px){
+        display: block;
+        overflow-x: scroll;
+        width: 100%;
+        padding: 4px;
+        font-size: 8pt;
+        align-items: center;
+    }
+
+    
+    th{
+        padding: 12px;
+        font-weight: 700;
+        background-color: gainsboro;
+    }
+    
+    td{
+        width: 200px;
+        justify-content: center;
+        text-align: center;
+        padding: 8px;       
+    }
+
+
+`
