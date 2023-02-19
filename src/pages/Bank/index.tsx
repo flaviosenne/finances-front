40917594e2b6c5dbc83776a2f031dd88dd7a475f
joao flavio <flaviosenne123@gmail.com>
2023-02-19 12:00:12 -0300

    criar componente de modal para excluir um item

--- src/pages/Bank/index.tsx
+++ src/pages/Bank/index.tsx
@@ -1,26 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Button from '../../components/Button'
+import Modal from '../../components/Modal'
 import SideBar from '../../components/SideBar'
 import Table from '../../components/Table'
 import { formatValueDate } from '../../utils/format-value-date'
-import { AddIcon, ButtonAdd, ButtonContainer, ButtonFilter, Container, Content, EditIcon, FilterIcon, TrashIcon } from './styles'
+import {
+    AddIcon, ButtonModal, ButtonAdd, ButtonContainer, ButtonFilter,
+    Container, ContentModal, Content, EditIcon, FilterIcon, TrashIcon
+} from './styles'
 
 export default function Bank() {
 
+    const [openModal, setOpenModal] = useState(false)
+    const [idToExclude, setIdToExclude] = useState('')
     const mockResult = [
         { date: '2022-01-12', status: 'Ativo', description: 'Santander', id: 'aaa' },
-        { date: '2022-07-14', status: 'Inativo', description: 'Nubank', id: 'aaa' },
-        { date: '2022-02-12', status: 'Ativo', description: 'Caixa', id: 'aaa' },
+        { date: '2022-07-14', status: 'Inativo', description: 'Nubank', id: 'bbb' },
+        { date: '2022-02-12', status: 'Ativo', description: 'Caixa', id: 'ccc' },
     ]
 
     const headerTable = ['Data Criação', 'Status', 'Descrições', 'Ações']
+
+    function handleExcludeItem(id: string) {
+        setOpenModal(true)
+        setIdToExclude(id)
+    }
+    
+    function removeItem(id: string){
+        setOpenModal(false)
+        alert('item exlcuido  '+ id)
+        setIdToExclude('')
+    }
 
 
     return (
         <Container>
             <SideBar />
-            <Content>
 
+            <ContentModal display={openModal}>
+
+                <Modal closeButton={true} title='Excluir item' height={150} width={400}>
+                    <>
+                        <span>Você tem certeza que deseja excluir esse item?</span>
+                        <ButtonModal>
+                            <Button title='Sim' height={30} onClick={() => removeItem(idToExclude)} />
+                            <Button title='Não' height={30} onClick={() => setOpenModal(false)} />
+                        </ButtonModal >
+                    </>
+                </Modal>
+            </ContentModal>
+
+            <Content>
                 <ButtonContainer>
                     <Link to='/banco/novo'>
                         <ButtonAdd>
@@ -36,21 +67,21 @@ export default function Bank() {
 
                 </ButtonContainer>
                 <Table header={headerTable}>
-                        <tbody>
-                            {mockResult.map(result => (
-                                <tr>
-                                    <td>{formatValueDate(result.date)}</td>
-                                    <td>{result.status}</td>
-                                    <td><p>{result.description}</p></td>
-                                    <td>
-                                        <Link to={`detalhes/${result.id}`}>
-                                            <EditIcon />
-                                        </Link>
+                    <tbody>
+                        {mockResult.map(result => (
+                            <tr>
+                                <td>{formatValueDate(result.date)}</td>
+                                <td>{result.status}</td>
+                                <td><p>{result.description}</p></td>
+                                <td>
+                                    <Link to={`detalhes/${result.id}`}>
+                                        <EditIcon />
+                                    </Link>
 
-                                        <TrashIcon /></td>
-                                </tr>
-                            ))}
-                        </tbody>
+                                    <TrashIcon onClick={() => handleExcludeItem(result.id)} /></td>
+                            </tr>
+                        ))}
+                    </tbody>
                 </Table>
             </Content>
         </Container>
--- src/pages/Bank/styles.ts
+++ src/pages/Bank/styles.ts
@@ -35,6 +35,37 @@ export const Content = styled.div`
     
 `
 
+interface PropsModal {
+    display: boolean
+}
+
+export const ContentModal = styled.div<PropsModal>`
+    width: 100%;
+    height: 100%;
+    display: ${props => props.display ? 'flex' : 'none'};
+    justify-content: center;
+    position: absolute;
+    background: rgba(0,0,0,0.7);
+    align-items: center;
+    align-self: center;
+    z-index: 1;
+    
+`
+
+
+export const ButtonModal = styled.div`
+    display: flex;
+    padding: 10px;
+    justify-content: space-around;
+    align-items: center;
+    width: 80%;
+    margin: 0 auto;
+    z-index: 1;
+    button{
+        margin: 10px;
+        width: 120px;
+    }
+`
 
 export const ButtonContainer = styled.div`
     display: flex;
